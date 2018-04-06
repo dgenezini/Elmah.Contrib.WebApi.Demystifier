050d88105128c088bc2bab0cdd94f6c4aa02c54a
Daniel Genezini <danielgenezini@gmail.com>
2018-04-06 18:32:34 -0300

    Initial commit

---
+++ src/Elmah.Contrib.WebApi.Demystifier/ElmahDemystifierExceptionLogger.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http.ExceptionHandling;
+
+namespace Elmah.Contrib.WebApi.Demystifier
+{
+    public class ElmahDemystifierExceptionLogger : ElmahExceptionLogger
+    {
+        public override void Log(ExceptionLoggerContext context)
+        {
+            // Retrieve the current HttpContext instance for this request.
+            HttpContext httpContext = GetHttpContext(context.Request);
+
+            if (httpContext == null)
+                return;
+
+            Exception ex = context.Exception.Demystify();
+
+            // Wrap the exception in an HttpUnhandledException so that ELMAH can capture the original error page.
+            Exception exceptionToRaise = new HttpUnhandledException(ex.Message, ex);
+
+            // Send the exception to ELMAH (for logging, mailing, filtering, etc.).
+            ErrorSignal signal = ErrorSignal.FromContext(httpContext);
+            signal.Raise(exceptionToRaise, httpContext);
+        }
+
+        private static HttpContext GetHttpContext(HttpRequestMessage request)
+        {
+            if (request == null)
+                return null;
+
+            object value;
+            if (request.Properties.TryGetValue("MS_HttpContext", out value))
+            {
+                HttpContextBase context = value as HttpContextBase;
+                if (context != null)
+                    return context.ApplicationInstance.Context;
+            }
+
+            return HttpContext.Current;
+        }
+    }
+}
---
+++ src/Elmah.Contrib.WebApi.Demystifier/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Elmah.Contrib.WebApi.Demystifier")]
+[assembly: AssemblyDescription("ElmahExceptionLogger for WebAPI using Ben.Demystifier for high performance understanding for stack traces")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Daniel Genezini")]
+[assembly: AssemblyProduct("Elmah.Contrib.WebApi.Demystifier")]
+[assembly: AssemblyCopyright("Copyright © 2018 Daniel Genezini")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e0fa99a6-f9c9-4ab3-ab2a-c516446e3da8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
